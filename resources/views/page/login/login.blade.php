7d96d12dcc91a47b0c8bbfea64b9d50b9fb9d52f
kadek-ar <ikadekarya020@gmail.com>
2022-06-27 21:33:43 +0700

    change-pwa-package

--- resources/views/page/login/login.blade.php
+++ resources/views/page/login/login.blade.php
@@ -6,7 +6,10 @@
  	<script defer src="apps.js"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-	@laravelPWA
+	<!-- PWA  -->
+	<meta name="theme-color" content="#6777ef"/>
+	<link rel="apple-touch-icon" href="{{ asset('/images/icons/logops-512x512.png') }}">
+	<link rel="manifest" href="{{ asset('/manifest.json') }}">
  </head>
  <body>
  <div class="bg-white login_box">
@@ -63,5 +66,13 @@
 
 	</form>
  </div>
+	<script src="{{ asset('/sw.js') }}"></script>
+	<script>
+		if (!navigator.serviceWorker.controller) {
+			navigator.serviceWorker.register("/sw.js").then(function (reg) {
+				console.log("Service worker has been registered for scope: " + reg.scope);
+			});
+		}
+	</script>	
  </body>
  </html>
