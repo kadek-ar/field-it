e3a74876226394cac9aaeabbba0dee64a0369b66
kadek-ar <ikadekarya020@gmail.com>
2022-07-03 04:24:06 +0700

    up

--- resources/views/page/login/signup.blade.php
+++ resources/views/page/login/signup.blade.php
@@ -3,6 +3,7 @@
  <head>
  	<title>Sign Up</title>
  	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="icon" href="{{ asset('/images/icons/logops-512x512.png') }}">
  	<script type="text/javascript" src="resources/js/apps.js"></script>
 	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
